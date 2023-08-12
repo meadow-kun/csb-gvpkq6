d43f998c27772d3640628274876330ba5e721950
Code Sandbox <hello@codesandbox.io>
2023-08-12 21:08:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b30659a3906b4bc3df29c569f2a04b42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab49fbf0401330a53b39910ec6869294",
+    "url": "https://.github.io/csb-gvpkq6/index.html"
+  },
+  {
+    "revision": "217d9b36691702734c8e",
+    "url": "https://.github.io/csb-gvpkq6/static/css/2.451ef5c3.chunk.css"
+  },
+  {
+    "revision": "bed98a5b4dc429d5a78a",
+    "url": "https://.github.io/csb-gvpkq6/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "217d9b36691702734c8e",
+    "url": "https://.github.io/csb-gvpkq6/static/js/2.a05020ad.chunk.js"
+  },
+  {
+    "revision": "bed98a5b4dc429d5a78a",
+    "url": "https://.github.io/csb-gvpkq6/static/js/main.b72364bf.chunk.js"
+  },
+  {
+    "revision": "0cf3f2ee91afe1231301",
+    "url": "https://.github.io/csb-gvpkq6/static/js/runtime~main.27a3f4a5.js"
+  },
+  {
+    "revision": "6923d8d0353d9405278be43882bf0931",
+    "url": "https://.github.io/csb-gvpkq6/static/media/microphone~MMbrAzIj.6923d8d0.svg"
+  }
+]);
